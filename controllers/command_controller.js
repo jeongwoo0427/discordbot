1ac66e485e0d66a33364ede15c49cb3adb279525
jeongwoo0427 <jwk90226482@gmail.com>
2022-09-07 18:42:54 +0900

    fix: 오타수정

--- controllers/command_controller.js
+++ controllers/command_controller.js
@@ -14,7 +14,7 @@ const commandController = async(message)=>{
                     .setTitle('커맨드 명령 v1.0.4')
                     .setDescription('( ~ 다음에 아래 명령어 중 선택해주세요.)')
                     .addFields([
-                        {name:"노래",value:"저의 예술적인 음악선율을 '감상'해 볼실게요. ^^"},
+                        {name:"노래",value:"저의 예술적인 음악선율을 '감상'해 보실게요. ^^"},
                         {name:"스플스케줄 또는 스플",value:"스플래툰의 스케줄을 보여준답니다."},
                         {name:"(기타) 널 사랑해 또는 안녕 겸사로봇? 또는 자기소개",value:"해보세요!"}
                     ]) 
