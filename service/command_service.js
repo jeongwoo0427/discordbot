e497375df1234c5212d5352c283cba86500ed942
jeongwoo0427 <jwk90226482@gmail.com>
2022-09-07 10:45:26 +0900

    fix: 명령어 접근문자 변경

--- service/command_service.js
+++ service/command_service.js
@@ -10,13 +10,13 @@ const commandService = async(message)=>{
                     embeds:[
                     new EmbedBuilder()
                     .setTitle('커맨드 명령어')
-                    .setDescription('(` 다음에 아래 명령어 중 선택해주세요.)')
+                    .setDescription('(~ 다음에 아래 명령어 중 선택해주세요.)')
                     .addFields([
                         {name:"노래",value:"저의 예술적인 음악선율을 '감상'해 볼실게요. ^^"},
                         {name:"스플스케줄 또는 스플",value:"스플 스케쥴을 보여준답니다."}
                     ])
                 ]});
-                
+
         }else if(command == '노래'){
             await message.channel.send('난나난나나난난 나나나난나~');
         }else if(command == '스플스케줄'||command == '스플스케쥴'||command.includes('스플')){
