6f515c7542facb261fb0e59f9f3769c5694dce89
jeongwoo0427 <jwk90226482@gmail.com>
2022-09-08 16:54:11 +0900

    fix: 인텐트 수정으로 보이스채널 상태 가져오기 완료

--- controllers/music_controller.js
+++ controllers/music_controller.js
@@ -1,18 +1,23 @@
 const { joinVoiceChannel } = require('@discordjs/voice');
 const dataController = require('../controllers/data_controller');
+const {Member} = require('discord.js');
  
 const musicController = async(message) =>{
-    try{
-        console.log(message.member.voice.channelId); //채널 아이디 확인용
- 
-        //if(message.member.voice == null) 
-        return message.channel.send('현재 TTS 기능 중비중입니다~~ ^^');
+    try {
+        console.log(message.member.voice.channel);
+        if (!message.member.voice.channel) {
+            return message.reply('TTS를 사용하기 위해 먼저 음성채널에 있어야 합니다.');
+        }
+     
+        //return message.channel.send('현재 TTS 기능 중비중입니다~~ ^^');
         
         const connection = joinVoiceChannel({  
-            channelId: message.member.voice.channelId,
+            channelId: message.member.voice.channel.id,
             guildId: message.guildId,
             adapterCreator: message.guild.voiceAdapterCreator
         }) 
+
+        setInterval(()=>{connection.disconnect();},5000); //초 뒤에 퇴장
     }catch(err){
         await dataController.insertErrorLog(err);
         message.channel.send('음성 모듈 관련 오류가 발생했습니다 ㅜㅜ');
