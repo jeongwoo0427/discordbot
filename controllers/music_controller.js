0512775d438f47fadebebf164d1c8249982e37f8
jeongwoo0427 <jwk90226482@gmail.com>
2022-09-13 18:00:01 +0900

    feat: requst로 카카오 TTS API 호출

--- controllers/music_controller.js
+++ controllers/music_controller.js
@@ -1,6 +1,9 @@
 const dataController = require('../controllers/data_controller');
-const discordTTS = require('discord-tts');
+//const discordTTS = require('discord-tts');
+const config = require('../config.json');
+const request = require('request');
 const {AudioPlayer, createAudioResource, StreamType, entersState, VoiceConnectionState, joinVoiceChannel} = require('@discordjs/voice');
+
  
 
 let voiceConnection;
@@ -15,12 +18,25 @@ const musicController = async(message) =>{
      
         //return message.channel.send('현재 TTS 기능 중비중입니다~~ ^^');
 
-        const stream = discordTTS.getVoiceStream('hello text to speech world');
-        const audioResource = createAudioResource(stream, {inputType:StreamType.Arbitrary,inlineVolume:true});
-        if(!VoiceConnectionState || VoiceConnection){
+        // const stream = discordTTS.getVoiceStream('hello text to speech world');
+        // const audioResource = createAudioResource(stream, {inputType:StreamType.Arbitrary,inlineVolume:true});
+        // if(!VoiceConnectionState || VoiceConnection){
             
+        // }
+        const xmlData = '<speak>테스트해보기.</speak>';
+        const options = {
+            uri : 'https://kakaoi-newtone-openapi.kakao.com/v1/synthesize',
+            method : 'POST',
+            headers: {
+                'Content-Type': 'application/xml',
+                Authorization: `KakaoAK ${config.kakao_token}`,
+            },
+            body: xmlData
         }
-        
+        request(options, (err,response,body)=>{
+            console.log(body);
+        });
+
         const connection = joinVoiceChannel({  
             channelId: message.member.voice.channel.id,
             guildId: message.guildId,
