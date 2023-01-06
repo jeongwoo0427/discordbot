263661fe93ff48d1f8d36c7dcb08036dea8bce87
jeongwoo0427 <jwk90226482@gmail.com>
2023-01-06 17:24:51 +0900

    feat: 겸사겸사 좀보이드 전용 TTS 추가

--- routes/message_route.js
+++ routes/message_route.js
@@ -1,18 +1,23 @@
 const commandController = require('../controllers/command_controller');
-const musicController = require('../controllers/music_controller');
+const voiceController = require('../controllers/voice_controller');
 const dataController = require('../controllers/data_controller');
 
 const messageRoute = async (message) =>{
     //console.log(message); 
     await dataController.insertMessage(message);
 
-    if(message.author.bot || !message.guild) return; 
+    if(message.author.id == '1016606382281199697')return voiceController.useBotTTS(message); //겸사서버 특수기능 : 좀보이드의 ID 일 경우 TTS로 이동
 
-    if(message.content == '겸사로봇') message.reply('~ 는 커맨드 관련 기능, * 은 TTS관련 기능을 사용합니다.');;
 
-    if(message.content.substring(0,1) == '~') commandController(message);
+    if(message.author.bot || !message.guild) return; //봇이거나 길드원이 아닐경우 리턴
 
-    if(message.content.substring(0,1) == `*`||message.content.substring(0,1) == `!`) musicController(message);
+    if(message.content == '겸사로봇')return message.reply('~ 는 커맨드 관련 기능, * 은 TTS관련 기능을 사용합니다.');;
+
+    if(message.content.substring(0,1) == '~')return commandController(message);
+
+    if(message.content.substring(0,1) == `*`||message.content.substring(0,1) == `!`)return voiceController.useUserTTS(message);
+
+    
 
 
     //일반 명령어
